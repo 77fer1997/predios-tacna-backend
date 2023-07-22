6fc555c2435d928806557f5719e6923a5caf388e
77fer1997 <77fer1997@gmail.com>
2023-07-21 23:08:01 -0500

    Agregando el script start en el package.json

--- config/mysql.js
+++ config/mysql.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 
 const pool = mysql
   .createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST || "127.0.0.1",
+    user: process.env.DB_USER || "luis",
+    password: process.env.DB_PASSWORD || "Minecraft958493106",
+    database: process.env.DB_DATABASE || "predios_tacna",
   })
   .promise();
 module.exports = pool;
